17165bf6bd9b5070bdf170f44d546dfa1b042674
Ariel Weisberg <aweisberg@apple.com>
2024-12-13 17:39:37 -0500

    Provide AccordConfigurationService.checkIfNodesRemoved with the set of live nodes and don't barrier for them if they are live, but there aren't any Accord tables

--- src/java/org/apache/cassandra/service/accord/AccordConfigurationService.java
+++ src/java/org/apache/cassandra/service/accord/AccordConfigurationService.java
@@ -55,11 +55,13 @@ import org.apache.cassandra.tcm.ClusterMetadataService;
 import org.apache.cassandra.tcm.Epoch;
 import org.apache.cassandra.tcm.listeners.ChangeListener;
 import org.apache.cassandra.tcm.membership.NodeId;
+import org.apache.cassandra.tcm.membership.NodeState;
 import org.apache.cassandra.utils.FBUtilities;
 import org.apache.cassandra.utils.Simulate;
 import org.apache.cassandra.utils.concurrent.AsyncPromise;
 import org.apache.cassandra.utils.concurrent.Future;
 
+import static org.apache.cassandra.service.accord.AccordTopology.tcmIdToAccord;
 import static org.apache.cassandra.utils.Simulate.With.MONITORS;
 
 // TODO: listen to FailureDetector and rearrange fast path accordingly
@@ -342,16 +344,22 @@ public class AccordConfigurationService extends AbstractConfigurationService<Acc
             }
         }
         reportTopology(topology);
-        if (epochs.lastAcknowledged() >= topology.epoch()) checkIfNodesRemoved(topology);
-        else epochs.acknowledgeFuture(topology.epoch()).addCallback(() -> checkIfNodesRemoved(topology));
+        Set<Node.Id> stillLiveNodes = metadata.directory.states.entrySet()
+                                                               .stream()
+                                                               .filter(e -> e.getValue() != NodeState.LEFT && e.getValue() != NodeState.LEAVING)
+                                                               .map(e -> tcmIdToAccord(e.getKey()))
+                                                               .collect(Collectors.toSet());
+        if (epochs.lastAcknowledged() >= topology.epoch()) checkIfNodesRemoved(topology, stillLiveNodes);
+        else epochs.acknowledgeFuture(topology.epoch()).addCallback(() -> checkIfNodesRemoved(topology, stillLiveNodes));
     }
 
-    private void checkIfNodesRemoved(Topology topology)
+    private void checkIfNodesRemoved(Topology topology, Set<Node.Id> stillLiveNodes)
     {
         if (epochs.minEpoch() == topology.epoch()) return;
         Topology previous = getTopologyForEpoch(topology.epoch() - 1);
         // for all nodes removed, or pending removal, mark them as removed so we don't wait on their replies
-        Sets.SetView<Node.Id> removedNodes = Sets.difference(previous.nodes(), topology.nodes());
+        Set<Node.Id> removedNodes = Sets.difference(previous.nodes(), topology.nodes());
+        removedNodes = Sets.filter(removedNodes, id -> !stillLiveNodes.contains(id));
         if (!removedNodes.isEmpty())
             onNodesRemoved(topology.epoch(), previous, removedNodes);
     }
