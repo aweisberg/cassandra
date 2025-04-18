486dca247ee38cae25ee1ef113b1c1d06434da7a
Ariel Weisberg <aweisberg@apple.com>
2025-04-18 17:45:16 -0400

    Remove transient from sstable metadata, repair, and compaction since it is no longer possible to have transient sstables.

--- src/java/org/apache/cassandra/db/compaction/PendingRepairManager.java
+++ src/java/org/apache/cassandra/db/compaction/PendingRepairManager.java
@@ -31,7 +31,6 @@ import java.util.stream.Collectors;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
